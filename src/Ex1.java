789807b80c6d8218ddfadf1cc144d5227eb0b854
Miguel José Rodrigues de Oliveira <miguel.roliveira.jose@gmail.com>
2026-01-06 07:07:36 -0300

    Exercício 1 Finalizado

---
+++ src/Ex1.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			int num;
+			
+			// ENTRADA DE DADOS
+			System.out.println("\nBEM-VINDO!!");
+			System.out.println("Digite um número para retornar a tabuada: ");
+			System.out.print(">>> ");
+			num = Integer.parseInt(scanner.nextLine());
+			
+			// LAÇO DE REPETIÇÃO DA TABUADA
+			for (int i = 1; i <= 10; i++) {
+				System.out.printf("\n%s x %s = %s", num, i, num * i);
+			}
+			
+			
+		} // TRY PRINCIPAL
+		
+
+	}
+
+}
